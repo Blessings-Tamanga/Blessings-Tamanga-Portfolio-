5c01233080692e9dbca9c7dc40a5df6dfecbc4cc
Blessings-Tamanga <blessingstamanga@gmail.com>
2025-12-07 06:29:13 +1400

    login modified

--- admin_login.php
+++ admin_login.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     
     // Simple authentication
-    if ($username === 'admin' && $password === 'admin123') {
+    if ($username === 'Alitheia' && $password === '$Minister@123') {
         $_SESSION['admin_logged_in'] = true;
         $_SESSION['admin_username'] = $username;
         header('Location: admin_dashboard.php');
@@ -149,11 +149,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <button type="submit" class="btn">Login</button>
         </form>
         
-        <div class="credentials">
-            <strong>Default Credentials:</strong><br>
-            Username: admin<br>
-            Password: admin123
-        </div>
+       
     </div>
 </body>
 </html>
